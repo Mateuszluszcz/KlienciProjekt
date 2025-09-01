ccc63ed4056c8dd0ab038af8ed5e0890a5b84da9
sdsa <sad>
2025-09-01 14:27:17 +0200

    Generator faktur

--- Models/Klient.cs
+++ Models/Klient.cs
@@ -10,22 +10,24 @@ namespace WebApplication2.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        
         public int ID { get; set; }
 
 
-
+        [Required(ErrorMessage = "Pole nie może być puste")]
         [DisplayName("Imię")]
         [RegularExpression(@"^[A-Za-zĄĆĘŁŃÓŚŻŹąćęłńóśżź]+$", ErrorMessage = "Imię może zawierać tylko litery.")]
         [StringLength(50, ErrorMessage = "Imię może mieć maksymalnie 50 znaków.")]
         public string Name { get; set; } = "";
-
+        [Required(ErrorMessage = "Pole nie może być puste")]
         [DisplayName("Nazwisko")]
         [RegularExpression(@"^[A-Za-zĄĆĘŁŃÓŚŻŹąćęłńóśżź]+$", ErrorMessage = "Nazwisko może zawierać tylko litery.")]
         [StringLength(50, ErrorMessage = "Nazwisko może mieć maksymalnie 50 znaków.")]
 
         public string Surname { get; set; } = "";
 
-        [Required]
+        [Required(ErrorMessage = "Pole nie może być puste")]
         [DisplayName("PESEL")]
         [RegularExpression(@"^[0-9]{11}$", ErrorMessage = "PESEL musi składać się z dokładnie 11 cyfr")]
         public string PESEL { get; set; } = "";
