b95824c25f54a00a43bf86aa726c7e96c46c8218
sdsa <sad>
2025-09-05 13:01:24 +0200

    poprawka faktur

--- obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcff642689c1ae41f73fa4336708d00e815368bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+262ebbd224c2e6f5d998687556fa819a89ff4395")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
